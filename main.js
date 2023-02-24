8fb7aa8bb21692c171baf2b825402700fe9fbc30
cesar_zavala <csr.jm.zvl@gmail.com>
2023-02-23 22:15:51 -0600

    Se hicieron cambios mayores en js para obtener estudiantes desde JSON, validar calificaciones numéricas, se creo string para ordenar los resultados y la función para mostrarla en una nueva ventana

--- main.js
+++ main.js
@@ -1,36 +1,71 @@
-const calculate = () => {
+// Función para calcular el promedio de notas
+const calculate = async () => {
+    // Estudiantes desde el archivo JSON
+    const response = await fetch('students.json');
+    const students = await response.json();
 
-    let espaniol = document.querySelector("#spanish").value;
-    let mate = document.querySelector("#math").value;
-    let eng = document.querySelector("#english").value;
-    let history = document.querySelector("#hist").value;
-    let grades = "";
-    
-    let totalgrades = parseFloat(espaniol) + parseFloat(mate) + parseFloat(eng) + parseFloat(history);
-    
-    let percentage = (totalgrades / 400) * 100;
-    if (percentage <= 100 && percentage >= 90) {
-        grades = "MUY BUEN DESEMPEÑO";
-    } 
-    else if (percentage <= 89 && percentage >= 80) {
-        grades = "BUEN DESEMPEÑO";
-    } 
-    else if (percentage <= 79 && percentage >= 60) {
-        grades = "REGULAR";
-    } 
-    else {
-        grades = "DEFICIENTE";
+    // Calificaciones a partir del formulario
+    const currentStudent = {
+        spanish: Number(document.querySelector("#spanish").value),
+        math: Number(document.querySelector("#math").value),
+        english: Number(document.querySelector("#english").value),
+        history: Number(document.querySelector("#hist").value),
+    };
+
+    // Validación calificaciones numéricas
+    if (Object.values(currentStudent).some(isNaN)) {
+        document.querySelector("#showdata").innerHTML = "Por favor ingrese notas válidas";
+        return;
     }
+
+    // Se usa POST para agregar el estudiante actual al JSON
+    await fetch('students.json', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(currentStudent)
+    });
     
-    if (espaniol == "" || mate == "" || eng == "" || history == "") {
-        document.querySelector("#showdata").innerHTML = "Favor de llenar todos los campos";
-    } 
-    else {    
-        if (percentage >= 59.9) {
-            document.querySelector("#showdata").innerHTML = `El porcentaje obtenido es de ${percentage}%. Tu nota es ${grades}. Tu estatus es APROBADO.`;
-        } 
-        else {
-            document.querySelector("#showdata").innerHTML = `El porcentaje obtenido es de ${percentage}%. Tu nota es ${grades}. Tu estatus es REPROBADO.`;
-        }
-    }
-}  
+    // Se calcula la suma de las calificaciones del estudiante actual, y se obtiene el promedio y el porcentaje correspondientes
+    const totalGrades = Object.values(currentStudent).reduce((sum, grade) => sum + Number(grade), 0);    
+    const getAverageGrade = () => totalGrades / 4;
+    const averageGrade = getAverageGrade();
+    const percentage = (totalGrades / 400) * 100;
+
+    // Se define un sistema de calificación con diferentes rangos y notas
+    const gradingSystem = [    
+        {range: [90, 100], grade: "A"},
+        {range: [80, 89], grade: "B"},
+        {range: [70, 79], grade: "C"},
+        {range: [60, 69], grade: "D"},
+        {range: [0, 59], grade: "F"},
+    ];
+
+    // Rango correspondiente al porcentaje obtenido y se obtiene la nota correspondiente
+    const { grade } = gradingSystem.find(({ range }) => percentage >= range[0] && percentage <= range[1]);
+
+    // Se determina si el estudiante aprobó o reprobó
+    const status = percentage >= 60 ? "APROBADO" : "REPROBADO";
+
+    // String con los resultados de las calificaciones del estudiante actual
+    const results = `
+        <p>Español: ${currentStudent.spanish}</p>
+        <p>Matemáticas: ${currentStudent.math}</p>
+        <p>Inglés: ${currentStudent.english}</p>
+        <p>Historia: ${currentStudent.history}</p>
+        <p>Promedio: ${averageGrade}</p>
+        <p>Porcentaje: ${percentage}%</p>
+        <p>Nota: ${grade}</p>
+        <p>Estado: ${status}</p>
+    `;
+
+     // Nueva ventana
+    const newWindow = window.open("", "Resultado", "width=400,height=400");
+
+    // Resultado en la nueva ventana
+    newWindow.document.write(`
+        <h1>Resultado</h1>
+        ${results}
+        `);
+};
